801d2c5fda128e1037ed7127d766d246d7d4588e
Myraan2 <samiraanderson24@gmail.com>
2024-03-21 11:04:13 +0100

    uppdaterat

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('goToPageButton').addEventListener('click', function() {
+    // Ändra webbläsarens URL till den angivna sidan
+    window.location.href = 'login.html'; // Byt ut "sida2.html" mot den faktiska URL:en för den sida du vill länka till
+});
